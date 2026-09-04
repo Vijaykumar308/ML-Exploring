5a87334074860fac0e0acbd329828bd6ff9339c2
Vijay Kumar <308vijaykumar@gmail.com>
2026-09-04 18:55:21 +0530

    linear nn created

--- neuron_netword_model_lession_06.py
+++ neuron_netword_model_lession_06.py
@@ -1,11 +1,41 @@
 import torch
 
-# Simple single-layer neural network example using PyTorch.
-# This creates a linear model with 1 input and 1 output, then prints its parameters.
+X = torch.tensor([[1.0], [2.0], [3.0], [4.0]])
+y = torch.tensor([[2.0], [4.0], [6.0], [8.0]])
+
+
 model = torch.nn.Linear(1, 1)
 
-print(model)
+# print(model)
+# print(X)
+# print(y)
 
-print(model.weight)
-print(model.bias)
+loss_fn = torch.nn.MSELoss()
 
+# print(loss_fn)
+
+prediction = model(X)
+loss = loss_fn(prediction, y)
+
+# print("Prediction:", prediction)
+# print("Loss:", loss)
+
+optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
+
+loss.backward()
+optimizer.step()
+
+# print("Updated weight:", model.weight)
+# print("Updated bias:", model.bias)
+
+for epoch in range(1000):
+    prediction = model(X)
+    loss = loss_fn(prediction, y)
+
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+print("Final weight:", model.weight)
+print("Final bias:", model.bias)
+print("Final loss:", loss)
